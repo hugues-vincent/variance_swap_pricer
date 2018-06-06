d8589ccaffb5e0482fdf446a09f2600f1ffffb3e
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-06-06 23:12:10 +0200

    change normal cdf function

--- main.cpp
+++ main.cpp
@@ -62,14 +62,7 @@ double Gaussian_distribution::pdf(const double& x) const {
 
 // Cumulative density function
 double Gaussian_distribution::cdf(const double& x) const {
-    double k = 1.0/(1.0 + 0.2316419*x);
-    double k_sum = k*(0.319381530 + k*(-0.356563782 + k*(1.781477937 + k*(-1.821255978 + 1.330274429*k))));
-    
-    if (x >= 0.0) {
-        return (1.0 - (1.0/(pow(2*M_PI,0.5)))*exp(-0.5*x*x) * k_sum);
-    } else {
-        return 1.0 - cdf(-x);
-    }
+    return 0.5 * erfc(-x * M_SQRT1_2);
 }
 
 // Inverse cumulative distribution function (probit function)
