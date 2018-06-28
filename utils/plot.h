af22eed4bebe7339d84b739f4b7391cea49be372
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-06-28 19:08:05 +0200

    simple plot

--- utils/plot.h
+++ utils/plot.h
@@ -7,57 +7,28 @@
 using namespace std;
 using curve = std::vector<std::pair<double, double>>;
 
-void plot(vector<pair<double, double>> xy_pts, const double x_l, const double x_r, const double y_l, const double y_r, const string name="")  
+curve path_to_curve(const std::vector<double> path, const double T) 
 {
-    	Gnuplot gp;
+	curve model_curve = curve();
+	int N = path.size();
+	double x = 0, dt = T/N;
+	for(double j=0; j<N; j++) {
+		model_curve.push_back(std::make_pair(x, path[j]));
+		x += dt;
+	}
+	return model_curve;
+}
 
-		vector<pair<double, double> > xy_pts_A;
-		for(double x=-2; x<2; x+=0.01) {
-			double y = x*x*x;
-			xy_pts_A.push_back(make_pair(x, y));
-		}
-
-		vector<pair<double, double> > xy_pts_B;
-		for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
-			double theta = alpha*2.0*3.14159;
-			xy_pts_B.push_back(make_pair(cos(theta), sin(theta)));
-		}
-
-		gp << "set xrange ["<<x_l<<":"<< x_r<<"]\nset yrange ["<<y_l<<":"<< y_r<<"]\n" << "plot";
-		gp << gp.file1d(xy_pts) <<  "with lines title '"<< name <<"',"<< endl;
-
-	#ifdef _WIN32
-		// For Windows, prompt for a keystroke before the Gnuplot object goes out of scope so that
-		// the gnuplot window doesn't get closed.
-		cout << "Press enter to exit." << endl;
-		cin.get();
-	#endif 
-};
-void plot(std::vector<curve> xy_ptss, const double x_l, const double x_r, const double y_l, const double y_r, vector<string> names = vector<string>()) 
-{
-    	Gnuplot gp;
-    	int N = xy_ptss.size();
-    	if(names.empty())
-    		names =  vector<string>(N, "");
-		gp << "set xrange ["<<x_l<<":"<< x_r<<"]\n set yrange ["<<y_l<<":"<< y_r<<"]\n" << "plot";
-		for(int i(0); i<N; i++)
-		{
-			gp << gp.file1d(xy_ptss[i]) <<  "with lines title '"<< names[i]<<"',";			
-		}
-		gp << endl;			
-
-};
-void plot(std::vector<std::vector<double>> ys, const double T, vector<string> names = vector<string>()) 
+void plot(std::vector<std::vector<double>> paths, const double T, vector<string> names = vector<string>()) 
 {
 	double max_= 0 , min_= 0;
 	Gnuplot gp;
-	curve model_curve;
-	int nb_curve = ys.size();
+	int nb_curve = paths.size();
 	for(int i(0); i<nb_curve; i++)
 	{
-		for(double j=0; j<ys[i].size(); j++) {
-			max_ = max(ys[i][j],max_);
-			min_ = min(ys[i][j],min_);
+		for(double j=0; j<paths[i].size(); j++) {
+			max_ = max(paths[i][j],max_);
+			min_ = min(paths[i][j],min_);
 		}
 	}
 	max_ += 0.1;
@@ -68,14 +39,7 @@ void plot(std::vector<std::vector<double>> ys, const double T, vector<string> na
 	gp << "set xrange ["<<0<<":"<< T<<"]\n set yrange ["<<min_<<":"<< max_<<"]\n" << "plot";
 	for(int i(0); i<nb_curve; i++)
 	{
-		model_curve = curve();
-		int N = ys[i].size();
-		double x = 0;
-		for(double j=0; j<N; j++) {
-			model_curve.push_back(std::make_pair(x, ys[i][j]));
-			x += T/N;
-		}
-		gp << gp.file1d(model_curve) <<  "with lines title '"<< names[i]<<"',";	
+		gp << gp.file1d(path_to_curve(paths[i], T)) <<  "with lines title '"<< names[i]<<"',";	
 	}
 	gp << endl;			
 
