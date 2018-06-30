a211e828eb5eb65144dae09fc5ffb5020c78b7e4
hugues.vincent.ropert@tetha.pw <hugues.vincent.ropert@tetha.pw>
2018-06-30 14:57:04 +0200

    add concat vector function

--- utils/utils.h
+++ utils/utils.h
@@ -19,49 +19,68 @@ using namespace std;
 std::random_device rd{};
 std::mt19937 gen{rd()};
 
+// =============================
+// Simple utilities function
+// =============================
 double max(const double x, const double y){ return x>y? x : y;}
 double min(const double x, const double y){ return x<y? x : y;}
 
+template <typename T>  
+std::vector<T> concat(std::vector<std::vector<T>>& to_concat_vectors)  
+{  
+	std::vector<T> concat_vector = {};
+	for (std::vector<T> &to_concat_vector : to_concat_vectors)
+		for (T &to_concat_item : to_concat_vector)
+			concat_vector.push_back(to_concat_item);
+	return concat_vector;
+}  
+
+// =============================
+// Random utilities function
+// =============================
 double uniform_draw(const double a, const double b)
 {
-    std::uniform_int_distribution<> dis{0, 99};
+	std::uniform_int_distribution<> dis{0, 99};
 	return  dis(gen);
 }
 
 double gaussian_draw(const double  mu = 0, const double  sigma = 1)
 {
-    std::normal_distribution<> dis{mu, sigma};
-    return dis(gen);}
+	std::normal_distribution<> dis{mu, sigma};
+	return dis(gen);}
 
 
-void p(const std::vector<double> v)
-{
-	for(int i(0); i<v.size(); i++)
+// =============================
+// Display utilities function
+// =============================
+	void p(const std::vector<double> v)
 	{
-		cout << v[i] <<" ";
+		for(int i(0); i<v.size(); i++)
+		{
+			cout << v[i] <<" ";
+		}
+		cout << "\n";
 	}
-	cout << "\n";
-}
-void p(const string var_name)
-{
-	cout << var_name << "\n";
-}
-void p(const string txt, const double var_name)
-{
-	cout << txt << ": " << var_name << "\n";
-}
+	void p(const string var_name)
+	{
+		cout << var_name << "\n";
+	}
+	void p(const string txt, const double var_name)
+	{
+		cout << txt << ": " << var_name << "\n";
+	}
 
-void p(const double var_name)
-{
-	cout << var_name << "\n";
-}
-void p(const string txt, const int var_name)
-{
-	cout << txt << ": " << var_name << "\n";
-}
+	void p(const double var_name)
+	{
+		cout << var_name << "\n";
+	}
+	void p(const string txt, const int var_name)
+	{
+		cout << txt << ": " << var_name << "\n";
+	}
 
-void p(const int var_name)
-{
-	cout << var_name << "\n";
-}
+	void p(const int var_name)
+	{
+		cout << var_name << "\n";
+	}
 #endif
